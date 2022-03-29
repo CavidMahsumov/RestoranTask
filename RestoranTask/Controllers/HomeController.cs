685316624e0f362b469d4c85d3b86016caaba694
Cavid <mehsumovcavid@gmail.com>
2022-03-29 23:24:30 +0400

    First Simple Task

--- RestoranTask/Controllers/HomeController.cs
+++ RestoranTask/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace RestoranTask.Controllers
         {
             List<Drink> drinks = new List<Drink>
             {
-                new Drink{ Name ="Cola", Price=0.70},
+                new Drink{ ImgPath="img/coco.png", Name ="Cola", Price=0.70},
                 new Drink{ Name="Pepsi", Price=0.70},
                 new Drink{ Name="Fanta", Price=0.70}
 
@@ -31,15 +31,52 @@ namespace RestoranTask.Controllers
 
             List<FastFood> foods = new List<FastFood>
             {
-                new FastFood{ Name="Cartoffree", Price=2.20},
-                new FastFood{ Name="Cartoffree", Price=2.20},
-                new FastFood{ Name="Cartoffree", Price=2.20}
+                new FastFood{ Name="Free", Price=2.20},
+                new FastFood{ Name="Burger", Price=1.90},
+                new FastFood{ Name="Pizza", Price=4.20}
             };   
 
             var model = new MealListViewModel { drinks = drinks, meals = Meals, fastfoods = foods };
 
             return View(model);
 
+
+        }
+
+        public IActionResult Drinks()
+        {
+            List<Drink> drinks = new List<Drink>
+            {
+                new Drink{ ImgPath="img/coco.png", Name ="Cola", Price=0.70},
+                new Drink{ Name="Pepsi", Price=0.70},
+                new Drink{ Name="Fanta", Price=0.70}
+
+            };
+
+            return View(drinks);
+
+        }
+        public IActionResult FastFoods()
+        {
+            List<FastFood> foods = new List<FastFood>
+            {
+                new FastFood{ Name="Free", Price=2.20},
+                new FastFood{ Name="Burger", Price=1.90},
+                new FastFood{ Name="Pizza", Price=4.20}
+            };
+            return View(foods);
+        }
+
+        public IActionResult Meals()
+        {
+            List<Meal> Meals = new List<Meal>
+            {
+                new Meal{ Name="Dolma", Price=3.00},
+                new Meal{ Name="Ash", Price=5.00},
+                new Meal{ Name="Kabab", Price=3.20}
+            };
+
+            return View(Meals);
 
         }
     }
